dc4dfaaceba7ed2df6499b88eb469f5b99b3f87d
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-05-29 13:07:36 +0200

    Create 0-fizzbuzz.py

---
+++ 0x00-challenge/0-fizzbuzz.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Fix FizzBuzz
+"""
+import sys
+
+
+def fizzbuzz(n):
+    """
+    Prints the numbers from 1 to n, each separated by a space.
+
+    - For multiples of three, prints "Fizz" instead of the number.
+    - For multiples of five, prints "Buzz" instead of the number.
+    - For numbers which are multiples of both three and five, prints "FizzBuzz".
+
+    Args:
+        n (int): The upper limit of the range to print (inclusive).
+    """
+    if n < 1:
+        return
+
+    tmp_result = []
+    for i in range(1, n + 1):
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
